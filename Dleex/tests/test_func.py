cc0cf24fd618d7374dcf6ef8f9cb087ea2799b6d
Blurbeast <o.samuel@native.semicolon.africa>
2023-05-30 16:47:00 +0100

    TestIt

--- Dleex/tests/test_func.py
+++ Dleex/tests/test_func.py
@@ -1,5 +1,5 @@
 """Using pytest"""
-from Week1730th.function import multiply
+from Week1730th.function import multiply, subtract
 from unittest import TestCase
 from Week1730th.function import square
 
@@ -11,3 +11,9 @@ class Test(TestCase):
 
     def test_square_of_a_number(self):
         assert square(4) == 16
+
+    def test_subtraction(self):
+        assert subtract(4, 5) == - 1
+
+    def test_subtraction_again(self):
+        assert subtract(10, 5) == 5
