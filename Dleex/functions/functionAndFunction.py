1dfb0adcfad8f5558d893c9397e36d3acf15c634
Blurbeast <o.samuel@native.semicolon.africa>
2023-05-31 11:10:28 +0100

    TestIt

---
+++ Dleex/functions/functionAndFunction.py
@@ -0,0 +1,38 @@
+"""idan"""
+
+
+def double_function(func2: list, func1: list):
+    elements = []
+    func22 = set(func2)
+    func111 = set(func1)
+    return tuple(func22 & func111)
+    # for i in func1:
+    #     for n in func2:
+    #         if i == n:
+    #             elements.append(i)
+    # return tuple(elements)
+
+
+lst2 = [1, 4, 3]
+lst1 = [1, 4, 5]
+
+print(double_function(lst1, lst2))
+
+
+def floating_number(flt1: float, flt2: float):
+    if type(flt1) != float or type(flt2) != float:
+        raise TypeError("float allowed")
+    return float(flt1 + flt2)
+
+
+# print(floating_number(2.2, 7))
+def check_status(right):
+    return True
+
+
+def discount(price, discount):
+    if price <= 0 or discount <= 0:
+        raise TypeError("Zero value not allowed")
+    if type(price) != float or int or type(discount) != float or int:
+        raise ValueError("Integers allowed alone")
+    return price - (price * (discount / 100))
--- Dleex/tests/test_func.py
+++ Dleex/tests/test_func.py
@@ -1,7 +1,40 @@
 """Using pytest"""
+import pytest as pytest
+
+import functions
 from Week1730th.function import multiply, subtract
 from unittest import TestCase
 from Week1730th.function import square
+from functions.functionAndFunction import floating_number
+
+
+def test_float():
+    with pytest.raises(TypeError):
+        floating_number(7, 8)
+        floating_number(6.8, 9)
+
+
+def test_square():
+    assert square(4) == 16
+
+
+def test_check_state():
+    assertTrue: functions.functionAndFunction.check_status(4)
+
+
+def test_discount():
+    assert functions.functionAndFunction.discount(1000, 10) == 900
+    assert functions.functionAndFunction.discount(1000, 5) == 950
+
+
+def test_discount_errors():
+    with pytest.raises(TypeError):
+        functions.functionAndFunction.discount(0, 10)
+
+
+def test_discount_errors_again():
+    with pytest.raises(TypeError):
+        functions.functionAndFunction.discount("0", "o")
 
 
 class Test(TestCase):
@@ -17,3 +50,9 @@ class Test(TestCase):
 
     def test_subtraction_again(self):
         assert subtract(10, 5) == 5
+
+    def test_floating_function(self):
+        self.assertRaises(TypeError, floating_number, (7, 9))
+
+    # def test_float_number(self):
+    #     assert TypeError(floating_number(7, 9))
