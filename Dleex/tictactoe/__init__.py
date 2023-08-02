e3878da0e66f3c50e29f786a01c99d57b900794c
Blurbeast <o.samuel@native.semicolon.africa>
2023-08-02 08:18:15 +0100

    Amend

--- Dleex/tictactoe/__init__.py
+++ Dleex/tictactoe/__init__.py
@@ -1,33 +1,13 @@
 from tictactoe.Board import Board
-from tictactoe.Player import Player
+from tictactoe.Game import Game
 
-player = Player()
-player2 = Player()
+__game_boy = Game()
 board = Board()
 
 
 def main():
-    print(board.playGame())
-    player2.movePlayer()
-
-
-#
-# def main():
-#     try:
-#         player.setName(input("Enter player 1 name: "))
-#         player.setSign(input("Enter player 1 sign: "))
-#         player2.setName(input("Enter player 2 name: "))
-#         try:
-#             player2.setSign(input("Enter player 2 sign: "))
-#             if player.getSign() == player2.getSign():
-#                 raise ValueError("it has been taken by player 1")
-#         except ValueError as error2:
-#             print(error2)
-#     except ValueError as error:
-#         print(error)
-#
-#     print(player.getName(), "and sign is", player.getSign())
-#     print(player2.getName(), "and sign is", player2.getSign())
+    __game_boy.start_game()
+    board.showBoard()
 
 
 if __name__ == "__main__":
